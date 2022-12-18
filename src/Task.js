923d6ab5cb579b04509cd7a154f542e465783a4c
Fatrian <fatrianramadhan02@gmail.com>
2022-12-18 17:45:16 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Achmad Fatrian Romadhoni" // TODO: replace this
+const studentId = "FE2974582" // TODO: replace this
+
+export { url, studentName, studentId }
