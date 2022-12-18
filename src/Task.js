c0986a26bdb472aa1dac8fdc41741c172044a2a0
Fatrian <fatrianramadhan02@gmail.com>
2022-12-18 17:53:15 +0700

    second commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://assignment-fatrian.netlify.app/" // TODO: replace this
 const studentName = "Achmad Fatrian Romadhoni" // TODO: replace this
 const studentId = "FE2974582" // TODO: replace this
 
